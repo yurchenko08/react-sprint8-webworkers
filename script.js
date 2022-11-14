e736eba650743847f5d75f5535f224c73eb0df78
yurchenko08 <yurchenko08@gmail.com>
2022-11-14 22:52:18 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-let worker = new Worker("worker.js");
-worker.onmessage = (message) => {
-  console.log("value from worker" + message.data);
-  result.innerText = `Result:${message.data}`;
-};
 function calculate() {
+  let worker = new Worker("worker.js");
   const integer = +document.getElementById("inputNumber").value;
   const result = document.getElementById("result");
-
+  result.innerText = "Calculating...";
   if (result.innerText === "Calculating...") {
     worker.terminate();
     worker = new Worker("worker.js");
     worker.postMessage({ data: integer });
   } else if (integer) {
     worker.postMessage({ data: integer });
-    result.innerText = "Calculating...";
   }
   worker.onmessage = (message) => {
     result.innerText = `Result:${message.data}`;
