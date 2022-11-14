0791515ccd1952a12be37bf8f5f519012aa0f058
yurchenko08 <yurchenko08@gmail.com>
2022-11-14 22:55:41 +0200

    fixed

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ function calculate() {
   let worker = new Worker("worker.js");
   const integer = +document.getElementById("inputNumber").value;
   const result = document.getElementById("result");
-  result.innerText = "Calculating...";
-  if (result.innerText === "Calculating...") {
-    worker.terminate();
-    worker = new Worker("worker.js");
-    worker.postMessage({ data: integer });
-  } else if (integer) {
-    worker.postMessage({ data: integer });
-  }
   worker.onmessage = (message) => {
     result.innerText = `Result:${message.data}`;
   };
+
+  if (result.innerText === "Calculating...") {
+    worker.terminate();
+    worker = new Worker("worker.js");
+    worker.onmessage = (message) => {
+      result.innerText = `Result:${message.data}`;
+    };
+
+    worker.postMessage({ data: integer });
+  } else if (integer) {
+    result.innerText = "Calculating...";
+    worker.postMessage({ data: integer });
+  }
 }
